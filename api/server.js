5d339613407a009e16e90d5efdac647162822a92
Josh Hethcox <josh.hethcox@gmail>
2018-07-18 16:52:48 -0400

    completed stage-1

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const colorRoutes = require("./routes/colors");
 
 const app = express();
 app.use(cors({ credentials: true }));
+
+colorRoutes(app);
 
 app.get("/", (req, res) => res.send("5in1 API Server"));
 
